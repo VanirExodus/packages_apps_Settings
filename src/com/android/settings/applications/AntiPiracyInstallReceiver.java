3b649770083e71024952ec22e449a4dc56c7fbde
Dave Kessler <activethrasher00@gmail.com>
2015-06-29 00:44:32 -0400

    Anti-piracy support: automatically disable and remove blacklisted piracy apps
    registers a broadcast receiver that detects blacklisted package installs and also
    activates on ACTION_BOOT_COMPLETED to make sure they can't be sideloaded or flashed in
    recovery.
    
    If a package is detected a service starts that automatically disables and uninstalls the
    blacklisted piracy app then stops itself again using reflection of an internal method
    in package manager to do it the same way any other app is uninstalled.
    
    Change-Id: Ic4d52d48edb1d9f99f1f782f8f6dd4766ae10a3d

---
+++ src/com/android/settings/applications/AntiPiracyInstallReceiver.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.applications;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.settings.R;
+import com.android.settings.applications.AntiPiracyConstants;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+/*
+ *  Help protect developers by blacklisting known piracy apps.  To add support for an app
+ *  simply add the package name to the AntiPiracyConstants.PACKAGES and update the description.
+ */
+
+public class AntiPiracyInstallReceiver extends BroadcastReceiver {
+    private static final String TAG = "PACKAGE_INSTALL_RECEIVER";
+    private static final boolean DEBUG = AntiPiracyConstants.DEBUG;
+
+    @Override
+    public void onReceive(Context ctx, Intent intent) {
+        Intent notifyService = new Intent(ctx, AntiPiracyNotifyService.class);
+        if (DEBUG) Log.i(TAG, "install check event");
+
+        for (String app : AntiPiracyConstants.PACKAGES) {
+            if (DEBUG) Log.e(TAG, "PACKAGE " + app + " testing for install");
+            if (isInstalled(ctx, app)) {
+                Log.i(TAG, "Blacklisted packages found " + app);
+                if (!isServiceRunning(AntiPiracyNotifyService.class, ctx)) {
+					ctx.startService(notifyService);
+                    Toast.makeText(ctx, "Anti-piracy software activated", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+    }
+
+    boolean isServiceRunning(Class<?> serviceClass, Context ctx) {
+        ActivityManager manager = (ActivityManager) ctx.getSystemService(Context.ACTIVITY_SERVICE);
+        for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                if (DEBUG) Log.i(TAG, "Check service already running");
+                return true;
+            }
+        }
+        if (DEBUG) Log.i(TAG, "Check service not running");
+        return false;
+    }
+
+    boolean isInstalled(Context ctx, final String packageName) {
+        final PackageManager pm = ctx.getPackageManager();
+        String mVersion;
+        try {
+            mVersion = pm.getPackageInfo(packageName, 0).versionName;
+            if (mVersion.equals(null)) {
+                return false;
+            }
+        } catch (NameNotFoundException e) {
+            if (DEBUG) Log.e(TAG, "Package " + packageName + " NameNotFoundException" + e);
+            return false;
+        }
+        return true;
+    }
+}
